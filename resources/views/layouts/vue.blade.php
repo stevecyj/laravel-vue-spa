55664239f20bd4dc667c51e0be9398f608b72b51
stevecyj <ycsteven.tsao@gmail.com>
2021-08-20 16:20:45 +0800

    style:Nunito Font

--- resources/views/layouts/vue.blade.php
+++ resources/views/layouts/vue.blade.php
@@ -5,7 +5,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- CSS -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet"></link>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     <title>Laravel</title>
 
 </head>
